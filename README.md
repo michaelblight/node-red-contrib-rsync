2274b29ef9400922b6da70966552a27bf55e1a16
Michael Blight <michael@blight.co>
2019-08-31 18:15:45 +1000

    Basics working

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# node-red-contrib-rsync
-Node for rsyncwrapper
+# rsync for Node-Red
+
+This node is a wrapper around the ![rsyncwrapper](https://www.npmjs.com/package/rsyncwrapper) module, which is a wrapper around rsync.
+
+It requires that rsync is already installed. If using the Docker image for Node-Red, you can create your own image with rsync
+installed using the following dockerfile:
+
+**
+
+    FROM nodered/node-red-docker
+
+    USER root
+
+    RUN apt-get update && apt-get -y install rsync
+
+    CMD ["npm", "start", "--", "--userDir", "/data"]
+
+You can then create the image using the following command:
+
+    sudo docker build -t node-red-rsync .
+
+And then replace the reference to the standard Node-Red image "nodered/node-red-docker" with your newly created one "node-red-rsync".
--- rsync-wrapper.js
+++ rsync-wrapper.js
@@ -1,11 +1,57 @@
+const rsync = require('rsyncwrapper')
+
 module.exports = function(RED) {
-    function rsyncNode(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("rsync", rsyncNode);
+	function rsyncNode(n) {
+		RED.nodes.createNode(this, n);
+		this.source = n.source
+		this.destination = n.destination
+		this.verbose = n.verbose
+		this.recursive = n.recursive
+		var node = this;
+
+		node.on('input', function(msg) {
+			rsyncRun(node, msg);
+		});
+	}
+
+	RED.nodes.registerType("rsync", rsyncNode);
+
+	function rsyncRun(node, msg) {
+		var source = typesOrDefault(['string', 'array'], msg.source, node.source)
+		var destination = typesOrDefault(['string'], msg.destination, node.destination)
+		var verbose = trueOrDefault(msg.verbose, node.verbose === true)
+		var recursive = trueOrDefault(msg.recursive, node.recursive === true)
+
+		var args = []
+		if (verbose === true) args.push('-v')
+
+		var options = {
+			src: source,
+			dest: destination,
+			recursive: recursive,
+			args: args,
+		}
+
+		rsync(options, function(error, stdout, stderr, cmd) {
+			if (error !== null) msg.error = error
+			if (stdout !== "") msg.stdout = stdout
+			if (stderr !== "") msg.stderr = stderr
+			msg.cmd = cmd
+			if (error) {
+				node.send([null, msg])
+			} else {
+				node.send([msg, null])
+			}
+		})
+	}
+
+	function typesOrDefault(types, v, d) {
+		var t = typeof v
+		if (Array.isArray(v)) t = 'array'
+		return (types.includes(t)) ? v : d
+	}
+
+	function trueOrDefault(v, d) {
+		return (v === true) ? v : d
+	}
 }
